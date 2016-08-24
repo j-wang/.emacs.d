4473f30df38d1e13a28e7dd7359e0e77766527ae
James Wang <veriathon@gmail.com>
2016-08-24 12:34:33 -0700

    Removing centered-window-mode from default text settings

--- init.el
+++ init.el
@@ -36,8 +36,7 @@
 ;; Add margins
 (defun my-set-text-properties ()
   "Set margins in current buffer, and prettiness."
-  (centered-window-mode t)
-  (visual-line-mode 1)
+  (Visual-line-mode 1)
   (line-height 1.2))
 
 (add-hook 'text-mode-hook 'my-set-text-properties)
